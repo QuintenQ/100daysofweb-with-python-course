5f6153e18d2f8b9ffc5fb80b1ef57e73991ee552
Dirk Trompetter <dirktrompetter@gmail.com>
2019-05-24 21:42:09 +0200

    add two projects

---
+++ projects/google_clone/program/routes.py
@@ -0,0 +1,11 @@
+from  program import app
+from flask import render_template
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
